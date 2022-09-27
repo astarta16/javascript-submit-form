fd59fb54baacb699aae17d1d459c4c28177fc645
astarta16 <ngedeshuri2001@gmail.com>
2022-09-27 03:19:16 -0700

    feat: add js file to project,select nd revervation form elements in varianbles

---
+++ index.js
@@ -0,0 +1,7 @@
+const form = document.querySelector('#surwayform');
+const firstName = document.querySelector('#inputName');
+const lastName = document.querySelector('#inputLast');
+const address = document.querySelector('#inputAddress');
+const datePicker = document.querySelector('#date');
+const selectGender = document.querySelector('#gender');
+const textArea= document.querySelector('#note');
